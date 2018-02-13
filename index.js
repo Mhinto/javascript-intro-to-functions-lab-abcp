7ceceab07a2dd923273fd19f9fd79accdb692858
Michael Hinton <michael01hinton@gmail.com>
2018-02-13 21:56:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+console.log(shout.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(whisper.toLowerCase())
+}
